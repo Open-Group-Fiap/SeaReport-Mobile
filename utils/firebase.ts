5f66efd61331a6fc754029ffe651aee18e8f544f
cauarojas <cauarojas@hotmail.com>
2024-05-29 10:06:42 -0300

    added firebase credentials

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,15 +1,13 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
-};
+    apiKey: 'AIzaSyC9xwvYKRFJfQzjNGRu6LLtjqRV6P0-RYg',
+    authDomain: 'op-sea-report.firebaseapp.com',
+    projectId: 'op-sea-report',
+    storageBucket: 'op-sea-report.appspot.com',
+    messagingSenderId: '739609791064',
+    appId: '1:739609791064:web:e4c9c159337683e617182d',
+    measurementId: 'G-7W8NQJE2KP',
+}
 
-const app = initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig)
